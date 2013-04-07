0821f35467d496deba0866f4772911ef258d78a4
Adrian Espinosa <mortuuslordofdeads@gmail.com>
2013-04-07 21:01:56 +0300

    Update python.py

--- dailyprogrammer/easy/121/python.py
+++ dailyprogrammer/easy/121/python.py
@@ -1,7 +1,2 @@
 def byte_exchange(coin):
-    print 'COIN {0}'.format(coin)
-    print coin/2
-    print coin/3
-    print coin/4
-
     return 1 if coin == 0 else byte_exchange(coin/2) + byte_exchange(coin/3) + byte_exchange(coin/4)
